e6e81cb901f22161dbf757d6114c584e36699c88
Darwill <example@gmail.com>
2026-03-25 15:31:30 +0300

    Вывели значение файла test

--- index.php
+++ index.php
@@ -2,5 +2,8 @@
 $file = fopen("test.txt", 'w') or die("не удалось открыиь файл");
 $string = "Привет мир";
 fputs($file,$string);
+   $str = htmlentities(file_get_contents("test.txt"));
+echo $str;
+
 fclose($file);
 ?>
