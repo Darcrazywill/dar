25710288897c2a429eb99e2896696d30858e75ea
Darwill <example@gmail.com>
2026-04-02 11:21:11 +0300

    сделали класс и вывели 2 обьекта

--- index.php
+++ index.php
@@ -1,107 +1,24 @@
 <?php
-//$file = fopen("test.txt", 'w') or die("не удалось открыиь файл");
-//$string = "Привет мир";
-//fputs($file,$string);
-//   $str = htmlentities(file_get_contents("test.txt"));
-//echo $str;
-//
-//fclose($file);
-
-
-//if (!rename("test.txt", "mir.txt"))
-//    echo "Ошибка переименования файла";
-//else echo "Файл переименован";
-//
-
-//mkdir("/var/www/Кокорева.ru/folder");
-//if (!rename("mir.txt", "folder/mir.txt"))
-//    echo "Ошибка движения файла";
-//else echo "Файл перемещен";
-   
-//if (copy("folder/mir.txt", "folder/world.txt"))
-//    echo "Копия файла создана";
-//else echo "Ошибка копирования файла";
-
-//$file=filesize("folder/world.txt");
-//echo $file. " " . "в байтах" . "</br>";
-//echo $file/1024 . " " . "в мегабайтах" . "</br>";
-//echo $file/1024/1024 . " " . "в гигабайтах" . "</br>";
-
-//if (unlink("folder/world.txt"))
-//    echo "Файл удален";
-//else echo "Ошибка при удалении файла";
-
-//$file1 = "folder/world.txt";
-//$file2 = "folder/mir.txt";
-//if(file_exists($file1)==true)
-//{
-  //  if(file_exists($file2)==true)
-    //{
-      //  echo "оба файла существуют";
-    //}
-    //else 
-    //{
-      //  echo "world - есть, mir - нет";
-    //}
-//}
-//else
-//{
-  //  if(file_exists($file2)==true)
-   // {
-     //   echo "world - нет, mir - есть";
-    //}
-    //else 
-   // {
-     //   echo "оба файла отсутствуют";
-    //}
-//}
-
-//mkdir("/var/www/Кокорева.ru/test");
-//
-//if (!rename ( "/var/www/Кокорева.ru/test" , "/var/www/Кокорева.ru/www"))
-//{
-//echo "Ошибка переименования папки";
-//}
-//else echo "Папка переименован";
-
-//rmdir("/var/www/Кокорева.ru/www");
-
-//mkdir("/var/www/Кокорева.ru/test");
-
-//$mainPath = "/var/www/Кокорева.ru/test";
-//$folders = ["str1", "str2", "str3"];
-//
-//foreach ($folders as $folder) {
-//    $path = $mainPath . "/" . $folder;
-//
-//    if (!is_dir($path)) {
-//        if (!@mkdir($path, 0777)) {
-//            echo "Ошибка mkdir " . "<br>";
-//        } else {
-//            echo "Создано: $path<br>";
-//        }
-//    } else {
-//        echo "Уже существует: $path<br>";
-//    }
-//}
-
-$dir = getcwd(); 
- 
-if (is_dir($dir))
+class Worker
 {
-    if ($dh = opendir($dir)) 
-    {
-        while (($file = readdir($dh)) !== false)
-        {
-            if($file=='.' || $file=='..') continue;
-             $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+    public $name;
+    public $age;
+    public $salary;
 
-            if (is_file($dir . '/' . $file) && $ext === 'jpeg') {
-                echo "jpеg-файл: $file <br>";
-            }
-        }
-        closedir($dh); 
+    public function __construct($name, $age, $salary)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->salary = $salary;
     }
 }
 
-?>
+$firstWorker = new Worker("Миша", 32, 1500);
+$secondWorker = new Worker("Маша", 20, 3500);
+
+echo "имя: " . $firstWorker->name . ", возраст: " . $firstWorker->age . ", зарплата: " . $firstWorker->salary;
+ 
+echo " имя: " . $secondWorker->name . ", возраст: " . $secondWorker->age . ", зарплата: " . $secondWorker->salary;
+
+?> 
+
